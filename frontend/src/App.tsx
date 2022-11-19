222bd58f4f69b0163f91c23b43b6db2dc0560d30
Rafael Fontana Becker Gois <rafael.gois@mendesmaquinas.com.br>
2022-11-18 22:42:10 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from "./components/header"
+import SalesCard from "./components/SalesCard"
 import NotificationButton from "./components/notificationButton"
 
 function App() {
-  return(
+  return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
