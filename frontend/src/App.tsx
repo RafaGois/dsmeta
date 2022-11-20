5a88b6e96bedd2ffe6385d9020a3db2ac8977683
Rafael Fontana Becker Gois <rafael.gois@mendesmaquinas.com.br>
2022-11-20 18:19:24 -0300

    toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,13 @@ import Header from "./components/header"
 import SalesCard from "./components/SalesCard"
 import NotificationButton from "./components/notificationButton"
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
