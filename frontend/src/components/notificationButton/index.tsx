154afe17b707326ba6db1c9aec68854446ad9501
Rafael Fontana Becker Gois <rafael.gois@mendesmaquinas.com.br>
2022-11-20 18:13:23 -0300

    notification

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,10 +1,26 @@
 import ico from '../../assets/img/notification-iconr.svg';
+import axios from 'axios';
 
 import './styles.css';
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+    
+}
+
+function handleClick (id : Number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log("SUCESSO!");
+        
+    })
+}
+
+function NotificationButton({saleId} : Props) {
+
     return(
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={ico} alt="Notificar" />
         </div>
     )
